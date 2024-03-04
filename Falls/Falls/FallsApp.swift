96d401126fb199a185f1df092707902c685aa67b
Filippo <rizzolofilippo18@gmail.com>
2024-03-04 13:10:07 +0100

    inserimento progetto

---
+++ Falls/Falls/FallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallsApp.swift
+//  Falls
+//
+//  Created by Filippo Rizzolo on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
